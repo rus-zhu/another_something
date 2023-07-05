a7a0c2f8129118f35d81c2e8053c08497484e9ca
rus-zhu <microlab982@yandex.ru>
2023-07-05 20:38:08 +0500

    create models User, Product, Order

---
+++ src/main/java/org/example/model/User.java
@@ -0,0 +1,67 @@
+package org.example.model;
+
+import java.util.Objects;
+
+public class User {
+    private int id;
+    private String first_name;
+    private String last_name;
+
+    public User() {
+    }
+
+    public User(int id, String first_name, String last_name) {
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != user.id) return false;
+        if (!Objects.equals(first_name, user.first_name)) return false;
+        return Objects.equals(last_name, user.last_name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (first_name != null ? first_name.hashCode() : 0);
+        result = 31 * result + (last_name != null ? last_name.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "User{id=%d, first_name='%s', last_name='%s'}".formatted(id, first_name, last_name);
+    }
+}
