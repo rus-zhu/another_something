7e2a743a428e13b7e1eac7c95fa7e38e1d883325
rus-zhu <microlab982@yandex.ru>
2023-07-05 20:58:31 +0500

    add DatabaseUtil and connection method

---
+++ src/main/java/org/example/repo/DatabaseUtil.java
@@ -0,0 +1,27 @@
+package org.example.repo;
+
+import org.example.config.DotenvConfig;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseUtil {
+
+    public static final String URL = DotenvConfig.Dotenv().get("URL");
+    public static final String USERNAME = DotenvConfig.Dotenv().get("USERNAME");
+    public static final String PASSWORD = DotenvConfig.Dotenv().get("PASSWORD");
+    public static final String DRIVER = DotenvConfig.Dotenv().get("DRIVER");
+
+    static {
+        try {
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection Connection() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
+}
